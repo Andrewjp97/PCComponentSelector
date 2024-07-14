58972a084f82ce8cf6b71f1f569328cd39d8dc8d
Andrew Paterson <Andrewjp97@me.com>
2024-07-14 09:22:25 -0400

    Converted to RAM component

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 1338;
 const app = express();
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/ram', async (req, res) => {
     try {
         const data = await pool.query('SELECT * FROM ram');
         res.status(200).send(data.rows);
@@ -15,22 +15,46 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.post('/', async (req, res) => {
-    const {name, location} = req.body;
+app.post('/ram', async (req, res) => {
+    const { name,
+            brand,
+            description,
+            dims,
+            speed,
+            ddrClass,
+            capacity,
+            photoURL,
+            microcenterLink,
+            amazonLink,
+            neweggLink,
+            bestbuyLink
+        } = req.body;
     try {
-        await pool.query('INSERT INTO ram (name, address) VALUES ($1, $2)', [name, location]);
-        res.status(200).send({message: "Succesfully added child to database"});
+        await pool.query('INSERT INTO ram (name, brand, description, dims, speed, ddrclass, capacity, photourl, microcenterlink, amazonlink, newegglink, bestbuylink) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', 
+            [name, brand, description, dims, speed, ddrClass, capacity, photoURL, microcenterLink, amazonLink, neweggLink, bestbuyLink]);
+        res.status(200).send({message: "Succesfully added RAM to database"});
     } catch (err) {
         console.error(err.message);
         res.sendStatus(500);
     }
 });
 
-app.get('/setup', async (req, res) => {
+app.get('/ram/setup', async (req, res) => {
     try {
-        await pool.query('CREATE TABLE ram (id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))');
+        await pool.query('CREATE TABLE ram (id SERIAL PRIMARY KEY, name VARCHAR(255), brand VARCHAR(255), description VARCHAR(2000), dims VARCHAR(255), speed VARCHAR(255), ddrclass VARCHAR(255), capacity VARCHAR(255), photourl VARCHAR(1000), microcenterlink VARCHAR(1000), amazonlink VARCHAR(1000), newegglink VARCHAR(1000), bestbuylink VARCHAR(1000))');
         res.status(200).send({message: "Succesfully created table"});
     } catch (err) {
+        console.error(err.message);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/ram/teardown', async (req, res) => {
+    try {
+        await pool.query('DROP TABLE ram');
+        res.status(200).send({message: "Succesfully dropped table"});
+    }
+    catch (err) {
         console.error(err.message);
         res.sendStatus(500);
     }
