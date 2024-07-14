ccc0d45685809556dc3977f5d1f7966882ba036b
Andrew Jacob Paterson <Andrewjp97@me.com>
2024-07-14 11:28:50 -0400

    Updated to have additional photo url's

--- server.js
+++ server.js
@@ -23,15 +23,19 @@ app.post('/ram', async (req, res) => {
             speed,
             ddrClass,
             capacity,
-            photoURL,
+            photoURL1,
+            photoURL2,
+            photoURL3,
+            photoURL4,
+            photoURL5,
             microcenterLink,
             amazonLink,
             neweggLink,
             bestbuyLink
         } = req.body;
     try {
-        await pool.query('INSERT INTO ram (name, brand, description, dims, speed, ddrclass, capacity, photourl, microcenterlink, amazonlink, newegglink, bestbuylink) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', 
-            [name, brand, description, dims, speed, ddrClass, capacity, photoURL, microcenterLink, amazonLink, neweggLink, bestbuyLink]);
+        await pool.query('INSERT INTO ram (name, brand, description, dims, speed, ddrclass, capacity, photourl1, photourl2, photourl3, photourl4, photourl5, microcenterlink, amazonlink, newegglink, bestbuylink) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)', 
+            [name, brand, description, dims, speed, ddrClass, capacity, photoURL1, photoURL2, photoURL3, photoURL4, photoURL5, microcenterLink, amazonLink, neweggLink, bestbuyLink]);
         res.status(200).send({message: "Succesfully added RAM to database"});
     } catch (err) {
         console.error(err.message);
@@ -41,7 +45,7 @@ app.post('/ram', async (req, res) => {
 
 app.get('/ram/setup', async (req, res) => {
     try {
-        await pool.query('CREATE TABLE ram (id SERIAL PRIMARY KEY, name VARCHAR(255), brand VARCHAR(255), description VARCHAR(2000), dims VARCHAR(255), speed VARCHAR(255), ddrclass VARCHAR(255), capacity VARCHAR(255), photourl VARCHAR(1000), microcenterlink VARCHAR(1000), amazonlink VARCHAR(1000), newegglink VARCHAR(1000), bestbuylink VARCHAR(1000))');
+        await pool.query('CREATE TABLE ram (id SERIAL PRIMARY KEY, name VARCHAR(255), brand VARCHAR(255), description VARCHAR(2000), dims VARCHAR(255), speed VARCHAR(255), ddrclass VARCHAR(255), capacity VARCHAR(255), photourl1 VARCHAR(1000), photourl2 VARCHAR(1000), photourl3 VARCHAR(1000), photourl4 VARCHAR(1000), photourl5 VARCHAR(1000), microcenterlink VARCHAR(1000), amazonlink VARCHAR(1000), newegglink VARCHAR(1000), bestbuylink VARCHAR(1000))');
         res.status(200).send({message: "Succesfully created table"});
     } catch (err) {
         console.error(err.message);
